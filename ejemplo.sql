365e93d77d16b43de5b8945f8a5fd6be75b3a2bb
Daniel <juan.bravo.g@uniautonoma.edu.co>
2025-03-25 10:25:39 -0500

    final

--- ejemplo.sql
+++ ejemplo.sql
@@ -1,12 +1,81 @@
-create table persona(
-    id_persona int primary key,
-    nombre varchar(75),
-    apellido varchar(75)
-);
+create table Estudiantes (
+  id int primary key,
+  nombre varchar(50)
+  );
 
-create table curso(
-    id_curso int primary key,
-    nombre varchar(25),
-    apellido varchar(25)
-);
+create table Estudiantes_matematicas (
+  id_e int,
+  FOREIGN key (id_e) REFERENCES Estudiantes(id)
+  );
+  
+create table Estudiantes_fisica (
+  id_e int,
+ FOREIGN key (id_e) REFERENCES Estudiantes(id)
+  );
+  
+create table Estudiantes_quimica (
+  id_e int,
+  FOREIGN key (id_e) REFERENCES Estudiantes(id)
+  );
+  
+  insert into Estudiantes_matematicas (id, nombre) VALUES
+  (1,'Ana'),
+  (2,'Carlos'),
+  (3,'Natalia'),
+  (4,'Valentina'),
+  (5,'Diego'),
+  (6,'Pam');
+  
+  insert into Estudiantes_quimica (id, nombre) VALUES
+  (1,'Diego'),
+  (2,'Valentina'),
+  (3,'Fernando'),
+  (4,'Gabriela'),
+  (5,'Ana'),
+  (6,'Jose');
+  
+  insert into Estudiantes_quimica (id, nombre) VALUES
+  (1,'Diego'),
+  (2,'Pam'),
+  (3,'Fernando'),
+  (4,'Mandy'),
+  (5,'Natalia'),
+  (6,'Larry');
 
+SELECT nombre from Estudiantes_matematicas
+UNION
+SELECT nombre from Estudiantes_fisica
+UNION
+SELECT nombre from Estudiantes_quimica;
+
+SELECT nombre from Estudiantes_matematicas
+UNION all
+SELECT nombre from Estudiantes_fisica
+UNION ALL
+SELECT nombre from Estudiantes_quimica;
+
+-- ///////////////////////////////////// --
+
+SELECT nombre from Estudiantes_matematicas
+intersect
+SELECT nombre from Estudiantes_fisica
+intersect
+SELECT nombre from Estudiantes_quimica;
+
+SELECT nombre from Estudiantes_fisica
+intersect
+SELECT nombre from Estudiantes_matematicas;
+
+-- ///////////////////////////////////// --
+
+SELECT nombre from Estudiantes_matematicas
+except
+SELECT nombre from Estudiantes_fisica
+except
+SELECT nombre from Estudiantes_quimica;
+
+SELECT nombre from Estudiantes_quimica
+except
+SELECT nombre from Estudiantes_fisica
+except
+SELECT nombre from Estudiantes_matematicas;
